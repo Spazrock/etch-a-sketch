d72662ee0f910213ef9e3005de0e2ceac563ecda
Ben Wilde <ben.wilde94@gmail.com>
2018-08-27 15:08:58 +1000

    Made grid in js, hover shade temporarily

---
+++ etch.js
@@ -0,0 +1,18 @@
+function genDivs(v){ 
+      const container = document.querySelector('#container'); 
+      for(i = 0; i < v; i++){ 
+        var row = document.createElement("div"); 
+        row.className = "row"; 
+        for(x = 1; x <= v; x++){ 
+            var cell = document.createElement("div"); 
+            cell.className = "gridsquare"; 
+			cell.classList.add('cell');
+            row.appendChild(cell); 
+        } 
+        container.appendChild(row); 
+      } 
+	  document.getElementById("container").innerHTML = container.innerHTML;
+    }
+
+	genDivs(16)
+	
